ea768daf7bc985b3042f608c6c1663bbe9d45d95
Maynor2021 <maynoravila19@gmail.com>
2025-10-31 00:17:32 -0600

    agregacion de entidades

---
+++ Control De Tareas/Data/Entitys/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Control_De_Tareas.Data.Entitys;
+
+namespace Control_De_Tareas.Data.Entitys
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+      //  public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
